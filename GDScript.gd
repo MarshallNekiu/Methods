1e256553563a15d82d443669cbbef8e26a07e019
Marshall <148552438+MarshallNekiu@users.noreply.github.com>
2024-03-14 17:28:47 -0300

    Update GDScript.gd

--- GDScript.gd
+++ GDScript.gd
@@ -8,18 +8,17 @@ func get_signals(node:Node, requested_method:Array[Callable] = [], show_lambdas 
 				if requested_method.is_empty() and not show_lambdas:
 					found[node].append(CONNECTION)
 					continue
-				
 				if CONNECTION["callable"] in requested_method or (show_lambdas and "<anonymous lambda>(lambda)" == str(CONNECTION["callable"])):
 					found[node].append(CONNECTION)
-					
+	
 	if found[node].is_empty(): found.erase(node)
 	
 	for i in node.get_children():
 		found.merge(await get_signals(i, requested_method, show_lambdas))
-		
+	
 	return found
 
 
-func pos_to_grid(node:Node, pos := get_global_mouse_position(), grid_step := Vector2(32, 32)):
-	node.global_position = Vector2((grid_step.x - int(pos.x) % int(grid_step.x)) + int(pos.x) , grid_step.y - (int(pos.y) % int(grid_step.y)) + pos.y)
+func pos_to_grid(pos: Vector2, grid_step := Vector2(32, 32)):
+	return Vector2((grid_step.x - int(pos.x) % int(grid_step.x)) + int(pos.x) , grid_step.y - (int(pos.y) % int(grid_step.y)) + pos.y)
 
